17350ccb2fb295bf7b479e1881ca413e232a7c16
N Chandu <chandunakkala0721@gmail.com>
2024-10-15 17:51:47 +0530

    this

---
+++ src/programm/program.java
@@ -0,0 +1,8 @@
+package programm;
+
+public class program {public program() {
+	// TODO Auto-generated constructor stub
+}public static void main(String[] args) {
+	System.out.println("iam the one who is ready to do ");
+}
+}
